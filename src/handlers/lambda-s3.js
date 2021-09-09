285c2d7a1e6cd0dafc7a90a4bb2342a7d5b24201
Fabio Palumbo <fabioppalumbo@gmail.com>
2021-09-09 03:10:13 -0300

    changes

--- src/handlers/lambda-s3.js
+++ src/handlers/lambda-s3.js
@@ -36,12 +36,6 @@ function createWriteStream(Bucket, Key) {
     return { writeStream, uploadPromise }
 }
 
-// perform count on line
-function processLine(line) {
-    // do something
-    return line
-}
-
 function convertYaml(file) {
     // do something
     return file
@@ -65,6 +59,12 @@ exports.lambdaS3Handler = async (event, context, callback) => {
             Key: record.s3.object.key,
         };
 
+        const sns_params = {
+            Message: 'This is a sample message',
+            Subject: 'Test SNS From Lambda',
+            TopicArn: process.env.SNS_ARN
+          };
+
         const src_bucket    = event.Records[0].s3.bucket.name;        
         const src_key       = event.Records[0].s3.object.key;
         const key           = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
@@ -73,14 +73,19 @@ exports.lambdaS3Handler = async (event, context, callback) => {
         const readStream = createReadline(event.inputBucket, event.inputKey)
         const { writeStream, uploadPromise } = createWriteStream(event.outputBucket, event.outputKey)
 
+        var path = require('path')
+        const type = path.extname(key)
 
         try {
             const { Body } = await s3.getObject(params).promise();
             console.log("File Name");
             console.log(key);
+            console.log(src_key);
             console.log("File Info");
             console.log(Body.toString());
 
+
+            // Tranforms + Count lines
             readStream.on('line', line => {
         
                 if (event.limit && event.limit <= totalLineCount) {
@@ -88,12 +93,21 @@ exports.lambdaS3Handler = async (event, context, callback) => {
                 }
                 
                 else {
-                    line = processLine(line)
-                    writeStream.write(`${line}\n`)
-                }
-        
+                    // Transform File
+                    if (type == '.yaml')
+                        line = convertYaml(key)
+                        writeStream.write(`${line}\n`)
+
+                    else if ( type == '.json')
+                        line = convertJson(key)
+                        writeStream.write(`${line}\n`)
+
+                    else
+                        const data = await sns.publish(sns_params).promise();
+                }        
                 totalLineCount++
-            })            
+            })
+
 
         } catch (error) {
             console.error('Error calling S3 getObject:', error);
